c39ef3f629e16da7e5b68273c8770fd011ec95a5
KharchenkoSanya <kharchenko1996oleksandr@gmail.com>
2022-12-06 15:34:56 +0100

    Ready clear HomeViewController

---
+++ Car Calculator/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Car Calculator
+//
+//  Created by Sashka IOS Developer on 06.12.2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
