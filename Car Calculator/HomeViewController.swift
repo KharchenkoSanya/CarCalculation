2ef0ecedfaffa79e0779577758415e5da3bcf84c
KharchenkoSanya <kharchenko1996oleksandr@gmail.com>
2022-12-06 17:24:51 +0100

    Ready clear HomeViewController

--- Car Calculator/HomeViewController.swift
+++ Car Calculator/HomeViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
